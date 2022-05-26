b17a660af3c8e84dc6be5a2b53765ba4edb3635c
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-05-26 21:01:33 +0300

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,19 @@
 import { swiper } from "./scripts/slider";
-import fullpage from "../../node_modules/fullpage.js";
+import fullpage from "../../node_modules/fullpage.js/dist/fullpage.js";
 
 new fullpage("#fullPage", {
   autoScrolling: true,
   scrollHorizontally: true,
+  scrollOverflow: true,
+  anchors: [
+    "top",
+    "products",
+    "benefits",
+    "specification",
+    "questions",
+    "contacts",
+  ],
+  menu: "#header__navigation",
 });
 
 const itemsTitle = document.querySelectorAll(".questions__item-title");
@@ -15,5 +25,19 @@ itemsTitle.forEach((item) => {
       item.classList.remove("questions__item--active");
     });
     event.target.parentNode.classList.add("questions__item--active");
+  });
+});
+
+const menuBtn = document.querySelector(".menu__btn");
+const menuList = document.querySelector(".menu__list");
+const menuListLink = document.querySelectorAll(".menu__list-link");
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("menu__btn--active");
+  menuList.classList.toggle("menu__list--active");
+});
+menuListLink.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuBtn.classList.remove("menu__btn--active");
+    menuList.classList.remove("menu__list--active");
   });
 });
